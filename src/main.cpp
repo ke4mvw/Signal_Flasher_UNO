8cd3c3f36168d754da032215e1fd2559510eeb7e
ke4mvw@gmail.com <ke4mvw@gmail.com>
2018-12-16 22:43:06 -0500

    individually controlled delays

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,19 @@
 #include <Arduino.h>
 int ledstate = 0b00100000; // initial state with bit 5 high;
+int delay1 = 500; //first delay - usually the same as second delay
+int delay2 = 500; //second delay - usually the same as first delay
 
 void setup() {
   DDRB |= 0b00110000; // set bits 5 and 4 as outputs
 }
 
 void loop() {
-  delay(500);
+  delay(delay1);
   ledstate &= 0b11001111; // BITWISE AND - change bit 5 and 4 to 0 - leave
                           // others unchanged
   PORTB =
       (ledstate |= 0b00100000); // BITWISE OR - change bit 5 to 1 - leave others unchanged
-  delay(500);
+  delay(delay2);
   ledstate &= 0b11001111; // BITWISE AND - change bit 5 and 4 to 0 - leave
                           // others unchanged
   PORTB =
