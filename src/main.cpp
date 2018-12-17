6236d67e8fbbe75ca6cda0df870d3ae73e118618
ke4mvw@gmail.com <ke4mvw@gmail.com>
2018-12-16 20:58:12 -0500

    Added Code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
+int ledstate = 0b00100000; //initial state with bit 5 high;
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+DDRB |= 0b00110000; //set bits 5 and 4 as outputs
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+    delay(500);
+    ledstate &= 0b11001111; //BITWISE AND - change bit 5 and 4 to 0 - leave others unchanged
+    PORTB = (ledstate |= 0b00100000); //BITWISE OR - change bit 5 to 1 - leave others unchanged
+    delay(500);
+    ledstate &= 0b11001111; //BITWISE AND - change bit 5 and 4 to 0 - leave others unchanged
+    PORTB = (ledstate |= 0b00010000); //BITWISE OR - change bit 4 to 1 - leave others unchanged
+ }
+   
