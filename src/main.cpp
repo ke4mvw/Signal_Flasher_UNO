99c0a4f039b61b78ee60ecbc31e0e8829dceedb3
ke4mvw@gmail.com <ke4mvw@gmail.com>
2018-12-22 16:44:46 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 int ledstate = 0b00100000; // initial state with bit 5 high;
-int delay1 = 500; //first delay - usually the same as second delay
-int delay2 = 500; //second delay - usually the same as first delay
+int delay1 = 500;          // first delay - usually the same as second delay
+int delay2 = 500;          // second delay - usually the same as first delay
 
 void setup() {
   DDRB |= 0b00110000; // set bits 5 and 4 as outputs
@@ -9,9 +9,15 @@ void setup() {
 
 void loop() {
   delay(delay1);
-  ledstate &= 0b11001111; // BITWISE AND - change bit 5 and 4 to 0 - leave others unchanged
-  PORTB = (ledstate |= 0b00100000); // BITWISE OR - change bit 5 to 1 - leave others unchanged
+  ledstate &= 0b11001111; // BITWISE AND - change bit 5 and 4 to 0 - leave
+                          // others unchanged
+  PORTB =
+      (ledstate |=
+       0b00100000); // BITWISE OR - change bit 5 to 1 - leave others unchanged
   delay(delay2);
-  ledstate &= 0b11001111; // BITWISE AND - change bit 5 and 4 to 0 - leave others unchanged
-  PORTB = (ledstate |= 0b00010000); // BITWISE OR - change bit 4 to 1 - leave others unchanged
+  ledstate &= 0b11001111; // BITWISE AND - change bit 5 and 4 to 0 - leave
+                          // others unchanged
+  PORTB =
+      (ledstate |=
+       0b00010000); // BITWISE OR - change bit 4 to 1 - leave others unchanged
 }
